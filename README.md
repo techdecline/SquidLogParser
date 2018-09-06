efea722ffa26d52cd85289681d72e5d265e69155
Schuchardt <schuchardt@softed.de>
2018-09-06 14:35:58 +0200

    documentation update and testing

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 PowerShell Module to parse Squid Log Files
 
 # Achievements
-2018-09-06 Ability to load Squid Access Log and return Target Url
+2018-09-06 Ability to load Squid Access Log and return all or selected parameters.
--- SquidLogParser.psm1
+++ SquidLogParser.psm1
@@ -174,15 +174,15 @@ function Open-SLPAccessLog {
    The return object will contain either all properties or only the ones that are selected using a parameter.
 .EXAMPLE
 
-   Resolve-SLPLogItem -LogFileLine "1536154946.720 126553 192.168.0.107 TCP_TUNNEL/200 8441 CONNECT cdn.onenote.net:443 - HIER_DIRECT/104.103.107.203 -"
+   Resolve-SLPLogItem -LogFileLine "1536154946.720 126553 192.168.0.107 TCP_TUNNEL/200 8441 CONNECT cdn.onenote.net:443 - HIER_DIRECT/104.103.107.203 -" -AllProperties
 
    This call will return an object containing all attributes.
 
 .EXAMPLE
 
-Resolve-SLPLogItem -LogFileLine "1536154946.720 126553 192.168.0.107 TCP_TUNNEL/200 8441 CONNECT cdn.onenote.net:443 - HIER_DIRECT/104.103.107.203 -" -Property TargetUrl
+    Resolve-SLPLogItem -LogFileLine "1536154946.720 126553 192.168.0.107 TCP_TUNNEL/200 8441 CONNECT cdn.onenote.net:443 - HIER_DIRECT/104.103.107.203 -" -Property TargetUrl
 
-This call will return an object containing only the target URL.
+    This call will return an object containing only the target URL.
 #>
 function Resolve-SLPLogItem {
     [CmdletBinding(DefaultParameterSetName="BySelectedProperties")]
